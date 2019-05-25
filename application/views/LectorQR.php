e3c8adabb8484540f61a908e630f3a040f930ba9
kevin <kevin.melendezlipes@gmail.com>
2019-05-25 16:30:13 -0300

    c3

--- application/views/LectorQR.php
+++ application/views/LectorQR.php
@@ -8,7 +8,23 @@
     <br><br></div>
   </div>
 </center>
-
+<div class="MainCont hidden">
+			<h1 class="Titulo">Tour Utec</h1>
+			<div class="panel panel-primary">
+				<form method="POST" action="<?php echo base_url('/Seccion'); ?>" name="formSeccion">
+					<div class="panel-header">
+						<h4 class="onH">Iniciar Sesion</h4>
+					</div>
+					<div class="panel-body">
+						<input name='ddlsec' id='ddlsec' type="text" placeholder="Usuario" class="form-control" /><br>
+						<input name='ddlidm' id='ddlidm' type="text" placeholder="Contraseña" class="form-control" />
+					</div>
+					<div class="panel-footer">
+						<button role="button" id="btn-buscar-sec" type="submit" class="btn">Ingresar</button>
+					</div>
+				</form>
+			</div>
+		</div>
 <script>
 let scanner = new Instascan.Scanner(
              {
@@ -18,7 +34,10 @@ let scanner = new Instascan.Scanner(
          scanner.addListener('scan', function(content) {
            var url = "<?php echo base_url('/seccion/');  ?>"
              //alert('Conenido: ' + url + 1);
-           window.open(url + content + "/1"); 
+             $("#ddlsec").val(content);
+             $("#ddlidm").val(1);
+          // window.open(url + content + "/1"); 
+          $("#btn-buscar-sec").click();
        });
          var ArrCamaras 
          var contCam = 0;
